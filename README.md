c6f84fe6b0fcbe8ea20dece7b7a14e4c75e79b83
AnkitaD <ankitadhyani.84@gmail.com>
2019-05-20 22:48:28 -0400

    updated portfolio

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ There are 3 html pages in this portfolio (creating the navigation bar)
 
 1.  index.html
     ----------
-    "index.htm" acts like the home page to the web-site, from where user can navigate to Portfolio and Contact pages. At the bottom of the page there is also a link to my "Resume" linked to a PDF file.
+    "index.htm" acts like the home page to the web-site, from where user can navigate to Portfolio and Contact pages. 
+    At the bottom of the page there is also a link to my "Resume" linked to a PDF file.
 
 2.  portfolio.html
     --------------
@@ -14,11 +15,14 @@ There are 3 html pages in this portfolio (creating the navigation bar)
     2.1.    Rutgers Bootcamp:
             Currently on-click on this button will generate a row of my projects that I have been part of in the Bootcamp.
             2.1.1.  VLTube Project
-            2.1.2.  Jersey Rail Project
-            2.1.3.  Guess The Flags Quiz Project
-            2.1.4.  Wonders of The World Quiz Project
-            2.1.5.  Crystal Collector Project
-            2.1.6.  Word Guess Game Project
+            2.1.2.  Jersey Rail App
+            2.1.3.  Guess The Flags Quiz App
+            2.1.4.  Wonders of The World Quiz App
+            2.1.5.  Crystal Collector App
+            2.1.6.  Word Guess Game App
+            2.1.7.  Burger App
+            2.1.8.  Note Taker App
+            2.1.9.  Inspire - Job Portal Project
 
             On-Click of any of the above projects will trigger the specific project to be open on a seperate window.
 
--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,4 +1,3 @@
-    
     //Bootcamp Projects object
     let bootcampProjects = [{
         projName: "VLTube",
@@ -24,69 +23,129 @@
         projName: "Word Guess Game",
         imgSrc: "../images/MakeAWord.png",
         refLink: "https://ankitadhyani.github.io/WordGuessGame/"
+    }, {
+        projName: "Burger App",
+        imgSrc: "../images/Sequelized-Burger.png",
+        refLink: "https://ankita-sequelize-burger.herokuapp.com/"
+    }, {
+        projName: "Note Taker",
+        imgSrc: "../images/NoteTaker_HomePage.png",
+        refLink: "https://note-taker-ankita.herokuapp.com/notes"
+    }, {
+        projName: "Inspire - Job Portal",
+        imgSrc: "../images/Inspire_JobPortal.png",
+        refLink: "https://inspire-job.herokuapp.com/"
     }];
 
 
     /* *****************************************************************************
-    * OnClick function that triggers when user clicks on Rutgers Bootcamp Projects
-    * ******************************************************************************/
+     * OnClick function that triggers when user clicks on Rutgers Bootcamp Projects
+     * ******************************************************************************/
 
     $("#rutgersProjectOnGithub").on("click", function () {
 
         console.log("Inside #rutgersProjectOnGithub onClick function");
 
+        $("#bootcampProjs").empty();
+
         for (let i = 0; i < bootcampProjects.length; i++) {
 
-            let $projDiv = $("<div>")
-                .addClass("col-2");
+            let $projDivCol = $("<div>");
+            $projDivCol
+                .addClass("col-4 mt-5 mb-5");
 
-            $("<p>")
-                .addClass("text-center pt-2")
-                .append(`<strong>${bootcampProjects[i].projName}</strong>`)
-                .css("color", "orangered")
-                .appendTo($projDiv);
 
-            let $proja = $("<a>")
+            let $aTag = $("<a>")
                 .addClass("nav-link")
                 .attr("href", bootcampProjects[i].refLink)
                 .attr("target", "_blank")
-                .appendTo($projDiv);
+                .appendTo($projDivCol);
 
-            let $projBtn = $("<button>")
-                .addClass("btn")
+
+            let $projimg = $("<img>");
+            $projimg
+                .addClass("card-img-top")
                 .attr("type", "button")
-                .css("position", "absolute")
-                .css("bottom", "2px")
+                .attr("src", bootcampProjects[i].imgSrc)
+                .attr("alt", bootcampProjects[i].projName)
                 .attr("width", "300px")
                 .attr("height", "300px")
                 .attr("data-toggle", "tooltip")
                 .attr("data-placement", "top")
                 .attr("title", bootcampProjects[i].projName+" Project")
-                .appendTo($proja);
-
-            let $projImg = $("<img>")
-                .addClass("img-thumbnail")
-                .attr("src", bootcampProjects[i].imgSrc)
-                .attr("alt", bootcampProjects[i].projName)
-                // .appendTo($proja);
-                .appendTo($projBtn);
-
+                .appendTo($aTag);
 
 
             $("#bootcampProjs")
-                .append($projDiv)
+                .append($projDivCol)
                 .css("border-radius", "10px")
                 .css("border", "3px dotted orangered")
                 .css("width", "100%")
-                .css("height", "200px")
                 .css("background-color", "rgba(255, 68, 0, 0.356)");
 
         }
     });
 
+
+
+    /*    $("#rutgersProjectOnGithub").on("click", function () {
+
+            console.log("Inside #rutgersProjectOnGithub onClick function");
+
+            let $projRow = $("<div>").addClass("row");
+
+            for (let i = 0; i < bootcampProjects.length; i++) {
+
+                let $projDiv = $("<div>")
+                    .addClass("col-3 p-3")
+                    .appendTo($projRow);
+
+                $("<p>")
+                    .addClass("text-center p-2 m-5")
+                    .append(`<strong>${bootcampProjects[i].projName}</strong>`)
+                    .css("color", "orangered")
+                    .appendTo($projDiv);
+
+                let $proja = $("<a>")
+                    .addClass("nav-link mt-5")
+                    .attr("href", bootcampProjects[i].refLink)
+                    .attr("target", "_blank")
+                    .appendTo($projDiv);
+
+                let $projBtn = $("<button>")
+                    .addClass("btn p-2")
+                    .attr("type", "button")
+                    .css("position", "absolute")
+                    .css("bottom", "2px")
+                    .attr("width", "300px")
+                    .attr("height", "300px")
+                    .attr("data-toggle", "tooltip")
+                    .attr("data-placement", "top")
+                    .attr("title", bootcampProjects[i].projName+" Project")
+                    .appendTo($proja);
+
+                let $projImg = $("<img>")
+                    .addClass("img-thumbnail")
+                    .attr("src", bootcampProjects[i].imgSrc)
+                    .attr("alt", bootcampProjects[i].projName)
+                    .appendTo($projBtn);
+
+
+
+                $("#bootcampProjs")
+                    .append($projDiv)
+                    .css("border-radius", "10px")
+                    .css("border", "3px dotted orangered")
+                    .css("width", "100%")
+                    // .css("height", "200px")
+                    .css("background-color", "rgba(255, 68, 0, 0.356)");
+
+            }
+        });
+    */
     /*************************************************************************************************
-    * A custom script is used to activate tooltips
-    *************************************************************************************************/
+     * A custom script is used to activate tooltips
+     *************************************************************************************************/
 
     $(function () {
         $('[data-toggle="tooltip"]').tooltip();
